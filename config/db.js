6aa30f8d33e78321fd46539b4a0146e78790cf09
Animesh Kalita <104593018+Ak-nut-47@users.noreply.github.com>
2023-12-16 17:19:36 +0530

    final changes

--- config/db.js
+++ config/db.js
@@ -1,21 +1,5 @@
-const mongoose = require("mongoose");
+const mongoose = require("mongoose")
 require("dotenv").config();
+const connection = mongoose.connect(process.env.mongoURL)
 
-
-const connectToDatabase = async () => {
-    try {
-        const connection = await mongoose.connect(process.env.mongoURL, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-
-        });
-
-        console.log("Connected to MongoDB");
-        return connection;
-    } catch (error) {
-        console.error("Error connecting to MongoDB:", error);
-        throw error;
-    }
-};
-
-module.exports = connectToDatabase;
+module.exports = connection;
